972a9fc065b277a82aaa399f1d29bb32a932ac31
Moritz <moritz.vogel3@gmail.com>
2017-04-30 21:59:54 +0200

    Added http interceptor for auth tests

--- test/auth.js
+++ test/auth.js
@@ -1,11 +1,41 @@
 'use strict';
 const chai = require('chai')
 const expect = chai.expect
+const nock = require('nock')
 const mojang = require('../')
 
 describe('mojang.auth()', () => {
 
-  // TODO implement test scenario with valid credentials 
+  before((done) => {
+
+    nock('https://authserver.mojang.com')
+      .post('/authenticate', {
+        username: 'valid@user.com'
+      })
+      .reply(200, {
+        accessToken: '123456abcdefghijklmnopqrstuvxyz',
+        clientToken: 'xxxxyyyyd6b94f58b7fa21a1189a62e4'
+      })
+      .post('/authenticate')
+      .reply(200, {
+        error: 'ForbiddenOperationException',
+        errorMessage: 'Invalid credentials. Invalid username or password.'
+      })
+      done()
+  })
+
+  it('should return an access token with valid credentials', (done) => {
+    mojang.auth('valid@user.com', 'password')
+      .then((tokens) => {
+        expect(tokens).to.have.property('accessToken')
+        expect(tokens).to.have.property('clientToken')
+        done()
+      })
+      .catch((err) => {
+        expect(err).to.be.null
+        done()
+      })
+  })
 
   it('should fail with invalid credentials', (done) => {
     mojang.auth('invalid@username.com', 'invalidpassword')
