c5e07e5df6902d025fd73ffccd45ebd227e74ed6
Moritz <moritz.vogel3@gmail.com>
2017-04-30 18:02:27 +0200

    Added basic test for authentification

---
+++ test/auth.js
@@ -0,0 +1,22 @@
+'use strict';
+const chai = require('chai')
+const expect = chai.expect
+const mojang = require('../')
+
+describe('authentification', () => {
+
+  // TODO implement test scenario with valid credentials 
+
+  it('should fail with invalid credentials', (done) => {
+    mojang.auth('invalid@username.com', 'invalidpassword')
+      .then((result) => {
+        expect(result).to.have.property('error')
+        done()
+      })
+      .catch((err) => {
+        expect(err).to.be.null
+        done()
+      })
+  })
+
+})
