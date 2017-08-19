76959f56fcceff7fce93e20e9cd104d7aa1e1085
Rob Mac <robmac@maccelerated.me>
2017-08-18 17:01:48 -0700

    Convert auth test to ava

--- test/authenticate.js
+++ test/authenticate.js
@@ -1,58 +1,38 @@
-/* eslint-env mocha */
-
-const {expect} = require('chai')
+const test = require('ava')
 const nock = require('nock')
 const authenticate = require('../lib/authenticate')
 
-describe('authenticate()', () => {
-  before((done) => {
-    // Behavior observed 17.08.2017 by maccelerated
-    nock('https://authserver.mojang.com')
-      .post('/authenticate', {
-        username: 'valid@user.com'
-      })
-      .reply(200, {
-        accessToken: '123456abcdefghijklmnopqrstuvxyz',
-        clientToken: 'xxxxyyyyd6b94f58b7fa21a1189a62e4'
-      })
+test('resolves with accessToken and clientToken', async t => {
+  // Behavior observed 17.08.2017 by maccelerated
+  nock('https://authserver.mojang.com')
+    .post('/authenticate', {
+      username: 'user@domain.tld',
+      password: 'user secret'
+    })
+    .reply(200, {
+      accessToken: '64e594b461a8c348a81005a43bcbf00d',
+      clientToken: 'ab1c5e7d2446675769c125ad494a290b'
+    })
 
-    // Behavior observed 17.08.2017 by maccelerated
-    nock('https://authserver.mojang.com')
-      .post('/authenticate', {
-        username: 'invalid@user.com'
-      })
-      .reply(403, {
-        error: 'ForbiddenOperationException',
-        errorMessage: 'Invalid credentials. Invalid username or password.'
-      })
-    done()
-  })
+  const session = await authenticate('user@domain.tld', 'user secret')
+  t.truthy(session.accessToken)
+  t.truthy(session.clientToken)
+})
 
-  after((done) => {
-    nock.cleanAll()
-    done()
-  })
+test('rejects with API\'s error on invalid credentials', async t => {
+  // Behavior observed 17.08.2017 by maccelerated
+  nock('https://authserver.mojang.com')
+    .post('/authenticate', {
+      username: 'user@domain.tld',
+      password: 'incorrect secret'
+    })
+    .reply(403, {
+      error: 'ForbiddenOperationException',
+      errorMessage: 'Invalid credentials. Invalid username or password.'
+    })
 
-  it('should return an access token with valid credentials', () => {
-    return authenticate('valid@user.com', 'password')
-      .then((session) => {
-        expect(session).to.have.property('accessToken')
-        expect(session).to.have.property('clientToken')
-      })
-  })
-
-  it('should fail with invalid credentials', () => {
-    return authenticate('invalid@user.com', 'password')
-      .then(() => {
-        throw new Error('should have rejected')
-      })
-      .catch((err) => {
-        expect(err).to.have.property('statusCode')
-        expect(err.statusCode).to.equal(403)
-        expect(err).to.have.property('name')
-        expect(err.name).to.equal('ForbiddenOperationException')
-        expect(err).to.have.property('message')
-        expect(err.message).to.equal('Invalid credentials. Invalid username or password.')
-      })
-  })
+  const err = await t.throws(authenticate('user@domain.tld', 'incorrect secret'))
+  t.is(err.statusCode, 403)
+  t.is(err.name, 'ForbiddenOperationException')
+  t.is(err.message, 'Invalid credentials. Invalid username or password.')
 })
