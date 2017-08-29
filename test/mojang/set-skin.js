093f3c3dbcdbd97b359bade9bf7d14497ee19404
Rob Mac <robmac@maccelerated.me>
2017-08-28 18:35:32 -0700

    Update setSkin test

--- test/mojang/set-skin.js
+++ test/mojang/set-skin.js
@@ -41,5 +41,25 @@ test('rejects when URL is bad', async t => {
       'errorMessage': 'Content is not an image'
     })
 
-  await t.throws(setSkin('goodaccesstoken', '7ddf32e17a6ac5ce04a8ecbf782ca509', 'badURL'))
+  const err = await t.throws(setSkin('goodaccesstoken', '7ddf32e17a6ac5ce04a8ecbf782ca509', 'badURL'))
+  t.is(err.name, 'IllegalArgumentException')
+  t.is(err.message, 'Content is not an image')
+})
+
+// API behavior observed 28.08.2017 by maccelerated
+test('rejects when access token is bad', async t => {
+  nock('https://api.mojang.com', {
+    reqheaders: {
+      'authorization': 'Bearer badaccesstoken',
+      'content-type': 'application/x-www-form-urlencoded'
+    }
+  }).post(`/user/profile/7ddf32e17a6ac5ce04a8ecbf782ca509/skin`, 'model=&url=urlOfPNG')
+    .reply(401, {
+      'error': 'Unauthorized',
+      'errorMessage': 'The request requires user authentication'
+    })
+
+  const err = await t.throws(setSkin('badaccesstoken', '7ddf32e17a6ac5ce04a8ecbf782ca509', 'urlOfPNG'))
+  t.is(err.name, 'Unauthorized')
+  t.is(err.message, 'The request requires user authentication')
 })
