7c8e110207d9bcc2db1813325cb162093e7294b1
Rob Mac <robmac@maccelerated.me>
2017-12-01 01:54:38 -0800

    Stop logging user agent

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,5 @@
 const {name, version, homepage} = require('../package.json')
 
-// TODO debug library, other?
-// console.debug is only available in node 8
-console.info('user-agent', `${name}/${version} (${homepage})`)
-
 exports.USER_AGENT = `${name}/${version} (${homepage})`
 exports.MOJANG_API = 'https://api.mojang.com'
 exports.STATUS_API = 'https://status.mojang.com'
